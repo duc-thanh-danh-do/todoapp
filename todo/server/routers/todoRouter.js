1f6e0814c65f9da9e64a051f840411b8e611834a
Danh Do <t3dodu00@students.oamk.fi>
2024-11-10 13:11:51 +0200

    update

--- todo/server/routers/todoRouter.js
+++ todo/server/routers/todoRouter.js
@@ -1,19 +1,10 @@
-//import { pool } from '../helpers/db.js'
 import { Router } from 'express'
-//import { emptyOrRows } from '../helpers/utils.js'
-//import { auth } from '../helpers/auth.js'
 import { getTasks, postTask, removeTask } from '../controllers/TaskController.js'
 
 const router = Router()
 
 router.get('/', getTasks)
-    /*(req,res, next) => {
-    pool.query('select * from task', (error, result)=> {
-        if (error) return next(error)
-        return res.status(200).json(emptyOrRows(result))
-    })
-}*/
-
+    
 router.post('/create', postTask)
 
 router.delete('/delete/:id', removeTask)
